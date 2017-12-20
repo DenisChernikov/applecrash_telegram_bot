cb775bdb41cddd7ea2d4ca759b78b1d7cf9c4f38
Denis Chernikov <work4den@gmail.com>
2017-12-20 05:44:24 +0300

    fixing all besides feedback logic

--- applecrash.py
+++ applecrash.py
@@ -37,7 +37,7 @@ class Users(Model):
         database = db
 
 # Активируем logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s -%(name)s -%(levelname)s -%(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Определяем переменные для состояний
@@ -45,22 +45,19 @@ DEVICE, IPHONE, IPAD, RESULT, ASK_INFO, END, BAD_END, CHOOSE, MENU, AGAIN = rang
 
 INFO = {}
 
-yes_no_keyboard = [[langs[user.lang]["yes"], langs[user.lang]["no"]], [langs[user.lang]["start_again"]]]
-yes_no_markup = ReplyKeyboardMarkup(yes_no_keyboard, one_time_keyboard=True)
-
 # Декоратор для проверки id пользователя
 def check_user(func):
     logger = logging.getLogger(func.__module__)
     @functools.wraps(func) #используем декоратор function.wraps для копирования информации об оборачиваемой функции
     def check(*args, **kwargs):
-        logger.info('Entering: %s', func.__name__)
+        logger.info('Entering: {}'.format(func.__name__))
         bot, update = args
         for a in args:
             logger.info(a)        
         user = Users.get(chat_id=update.message.chat_id)
         kwargs['user'] = user
         result = func(*args, **kwargs)
-        logger.info('Exiting: %s', func.__name__)
+        logger.info('Exiting: {}'.format(func.__name__))
         return result
     return check
 
@@ -82,11 +79,12 @@ def menu(bot, update, **kwargs):
         user.lang = "en_US"
     elif update.message.text == 'Русский':
         user.lang = "ru_RU"
+
     user.first_step_lang = "yes" # Маркер первого шага
     user.save()
 
     reply_keyboard = [[langs[user.lang]["know_the_price"]], [langs[user.lang]["promotions"]]]
-    update.message.reply_text(langs[user.lang]["hello"] % user.first_name, reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    update.message.reply_text(langs[user.lang]["hello"].format(user.first_name), reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return CHOOSE
 
 @check_user
@@ -140,57 +138,79 @@ def full_price(bot, update, **kwargs):
 @check_user
 def iphone(bot, update, **kwargs):
     user = kwargs['user']
-    reply_keyboard = [["5", "5c", "5s", "5se"], ["6", "6c", "6s", "6+", "6s+"], ["7", "7+", "8", "8+", "X"], [langs[user.lang]["start_again"]]]
     user.device = update.message.text
-    update.message.reply_text(langs[user.lang]["ask_model"] % user.device,
+    user.save()
+
+    reply_keyboard = [["5", "5c", "5s", "5se"], ["6", "6c", "6s", "6+", "6s+"], ["7", "7+", "8", "8+", "X"], [langs[user.lang]["start_again"]]]
+
+    update.message.reply_text(langs[user.lang]["ask_model"].format(user.device),
                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return IPHONE
     
 @check_user
 def ipad(bot, update, **kwargs):
     user = kwargs['user']
-    reply_keyboard = [["1", "2", "3", "4"], ["Air", "Air 2", "Mini", "Mini 2", "Mini 3"], [start_again]]
-    INFO["device"] = update.message.text
-    update.message.reply_text(ask_model % INFO["device"],
+    user.device = update.message.text
+    user.save()
+
+    reply_keyboard = [["1", "2", "3", "4"], ["Air", "Air 2", "Mini", "Mini 2", "Mini 3"], [langs[user.lang]["start_again"]]]
+
+    update.message.reply_text(langs[user.lang]["ask_model"].format(user.device),
                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return IPAD
-    
+
+@check_user
 def choice(bot, update, **kwargs):
-    user = Users.get(chat_id=update.message.chat_id)
+    user = kwargs['user']
     user.device_model = update.message.text
+    user.save()
+
     reply_keyboard = [[langs[user.lang]["screen"], langs[user.lang]["liquid"]], [langs[user.lang]["button"], langs[user.lang]["cam"]], [langs[user.lang]["mic"], langs[user.lang]["connector"]], [langs[user.lang]["other"], langs[user.lang]["I_dont_know"]], [langs[user.lang]["start_again"]]]
-    update.message.reply_text(langs[user.lang]["ask_fault"] % (user.device, user.device_model), reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    update.message.reply_text(langs[user.lang]["ask_fault"].format(user.device, user.device_model), reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return RESULT
 
 def result(bot, update, **kwargs):
     user = Users.get(chat_id=update.message.chat_id)
-    lang = user.lang
-    user.fault = update.message.text    
-    if user.fault == other or INFO["fault"] == idn:
-            update.message.reply_text(if_dont_know, reply_markup=yes_no_markup)
+    user.fault = update.message.text
+    for phrase in langs[user.lang]:
+        if user.fault == langs[user.lang][phrase]:
+            user.fault = phrase
+    user.save()
+
+    yes_no_keyboard = [[langs[user.lang]["yes"], langs[user.lang]["no"]], [langs[user.lang]["start_again"]]]
+    yes_no_markup = ReplyKeyboardMarkup(yes_no_keyboard, one_time_keyboard=True)
+
+    if user.fault == "other" or user.fault == "I_dont_know":
+            update.message.reply_text(langs[user.lang]["if_dont_know"], reply_markup = yes_no_markup)
     else:
         if user.device == "iPhone":
             result_text = faults_iphone[user.device_model][user.fault]
         elif user.device == "iPad":
             result_text = faults_ipad[user.device_model][user.fault]
-        if user.device == "iPhone" and (user.device_model == "8" or user.device_model == "8+" or INFO["device_model"] == "X"):
-            update.message.reply_text(iphone_after_eight % user.device_model, reply_markup=yes_no_markup)
+        if user.device == "iPhone" and (user.device_model == "8" or user.device_model == "8+" or user.device_model == "X"):
+            update.message.reply_text(langs[user.lang]["iphone_after_eight"].format(user.device_model), reply_markup=yes_no_markup)
         elif user.fault == "screen":
-            update.message.reply_text(result_with_screen % (user.fault, result_text[0], result_text[1], result_text[2]), reply_markup=yes_no_markup)
+            update.message.reply_text(langs[user.lang]["result_with_screen"].format(langs[user.lang][user.fault], langs[user.lang].get(result_text[0], result_text[0]), langs[user.lang][result_text[1]], langs[user.lang][result_text[2]]), reply_markup=yes_no_markup)
         else:
-            update.message.reply_text(resulting % (user.fault, result_text[0], result_text[1], result_text[2]), reply_markup=yes_no_markup)
+            update.message.reply_text(langs[user.lang]["resulting"].format(langs[user.lang][user.fault], langs[user.lang].get(result_text[0], result_text[0]), langs[user.lang][result_text[1]], langs[user.lang][result_text[2]]), reply_markup=yes_no_markup)
     return ASK_INFO
 
+@check_user
 def ask_info(bot, update, **kwargs):
-    contact_keyboard = KeyboardButton(text=send_contact, request_contact=True)
-    reply_keyboard = [[contact_keyboard, no_contact], [start_again]]    
-    update.message.reply_text(ask_contact,
+    user = kwargs['user']
+
+    contact_keyboard = KeyboardButton(text = langs[user.lang]["send_contact"], request_contact=True)
+    reply_keyboard = [[contact_keyboard, no_contact], [langs[user.lang]["start_again"]]]
+    update.message.reply_text(langs[user.lang]["ask_contact"],
                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return END
 
+@check_user
 def ask_again(bot, update, **kwargs):
-    reply_keyboard = [[price, terms], [necessity_contacts], [start_again]]
-    update.message.reply_text(so_sad, reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    user = kwargs['user']
+
+    reply_keyboard = [[langs[user.lang]["price"], langs[user.lang]["terms"]], [langs[user.lang]["necessity_contacts"]], [langs[user.lang]["start_again"]]]
+    update.message.reply_text(langs[user.lang]["so_sad"], reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     
     return BAD_END
 
@@ -204,40 +224,41 @@ def bad_end(bot, update, **kwargs):
     reply_keyboard = [[langs[user.lang]["start_again"]]]
     update.message.reply_text(langs[user.lang]["thank_you_bad"], reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
-    bot.sendMessage(chat_id = 130955703, text = 'Заказ от %s %s\nАппарат: %s %s\nНеисправность: %s\nЧто не устроило: %s\nСсылка на telegram: @%s\nchat_id: %s' % 
-                    (user.first_name, user.last_name, user.device, user.device_model, user.fault, user.what_bad, user.username, user.chat_id))
+    total_info = ('Заказ от {} {}\nАппарат: {} {}\nНеисправность: {}\nЧто не устроило: {}\nСсылка на telegram: @{}\nchat_id: {}'.format(user.first_name,
+                                user.last_name, user.device, user.device_model, langs[user.lang][user.fault], user.what_bad, user.nickname, user.chat_id))
 
-    bot.sendMessage(chat_id = 226052695, text = 'Заказ от %s %s\nАппарат: %s %s\nНеисправность: %s\nЧто не устроило: %s\nСсылка на telegram: @%s\nchat_id: %s' % 
-                    (user.first_name, user.last_name, user.device, user.device_model, user.fault, user.what_bad, user.username, user.chat_id))
+    for chatid in [130955703, 226052695]:
+        bot.sendMessage(chat_id=chatid, text = total_info)
+
     return ConversationHandler.END
 
 @check_user        
 def end(bot, update, **kwargs):
     user = kwargs['user']
     user.contact = update.message.text if update.message.text else "+" + update.message.contact.phone_number
-    reply_keyboard = [[start_again]]
-    update.message.reply_text(thank_you_good, reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    user.save()
 
-    bot.sendMessage(chat_id = 130955703, text = 'Заказ от %s %s\nАппарат: %s %s\nНеисправность: %s\nЧто не устроило: %s\nСсылка на telegram: @%s\nchat_id: %s' % 
-                    (user.first_name, user.last_name, user.device, user.device_model, user.fault, user.what_bad, user.username, user.chat_id))
+    reply_keyboard = [[langs[user.lang]["start_again"]]]
+    update.message.reply_text(langs[user.lang]["thank_you_good"], reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
-    bot.sendMessage(chat_id = 226052695, text = 'Заказ от %s %s\nАппарат: %s %s\nНеисправность: %s\nЧто не устроило: %s\nСсылка на telegram: @%s\nchat_id: %s' % 
-                    (user.first_name, user.last_name, user.device, user.device_model, user.fault, user.what_bad, user.username, user.chat_id))
-    return ConversationHandler.END
-    
+    total_info = ('Заказ от {} {}\nАппарат: {} {}\nНеисправность: {}\nЧто не устроило: {}\nСсылка на telegram: @{}\nchat_id: {}'.format(user.first_name,
+                                user.last_name, user.device, user.device_model, langs[user.lang][user.fault], user.what_bad, user.nickname, user.chat_id))
+
+    for chatid in [130955703, 226052695]:
+        bot.sendMessage(chat_id=chatid, text=total_info)
+        return ConversationHandler.END
+
+@check_user
 def cancel(bot, update, **kwargs):
-    reply_keyboard = [[start_again]]
+    user = kwargs['user']
+    reply_keyboard = [[langs[user.lang]["start_again"]]]
     update.message.reply_text(if_cancel, reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return ConversationHandler.END
 
 
-def check(bot, update, **kwargs):
-    user = Users.get(chat_id=update.message.chat_id)
-    return user, lang
-
 # Выводим в логи ошибки
 def error(bot, update, error):
-    logger.warn('Update "%s" caused error "%s"' % (update, error))
+    logger.warning('Update "{}" caused error "{}"'.format(update, error))
 
 
 def main():
@@ -263,12 +284,12 @@ def main():
             IPHONE: [RegexHandler('^(5|5c|5s|5se|6|6c|6s|6\+|6s\+|7|7\+|8|8\+|X)$', choice), RegexHandler('^Начать сначала$', start), MessageHandler(Filters.text, cancel)],
             IPAD: [RegexHandler('^(1|2|3|4|Air|Air 2|Mini|Mini 2|Mini 3)$', choice), RegexHandler('^Начать сначала$', start), MessageHandler(Filters.text, start)],
             RESULT: [RegexHandler('^(разбился экран|попала жидкость|сломалась кнопка|сломалась камера|сломался микрофон|сломался разъём|другое|я не знаю|the screen is broken|the button is broken|liquid got inside|cam is broken|mic is broken|connector is broken|other|I don\'t know)$', result), RegexHandler('^Начать сначала$', start), MessageHandler(Filters.text, start)],
-            ASK_INFO: [RegexHandler('^Да$', ask_info),
-                       RegexHandler('^Нет$', ask_again),
-                       RegexHandler('^Начать сначала$', start),
-                       MessageHandler(Filters.text, result)],
-            END: [MessageHandler(Filters.contact, end),  RegexHandler('^Начать сначала$', start), MessageHandler(Filters.text, ask_again)],
-            BAD_END: [ RegexHandler('^Начать сначала$', start), MessageHandler(Filters.text, bad_end)]
+            ASK_INFO: [RegexHandler('^(Да|Yes)$', ask_info),
+                       RegexHandler('^(Нет|No)$', ask_again),
+                       RegexHandler('^(Начать сначала|Start again)$', start),
+                       MessageHandler(Filters.text, start)],
+            END: [MessageHandler(Filters.contact, end),  RegexHandler('^(Начать сначала|Start again)$', start), MessageHandler(Filters.text, ask_again)],
+            BAD_END: [ RegexHandler('^(Начать сначала|Start again)$', start), MessageHandler(Filters.text, bad_end)]
         },
         
         fallbacks = [CommandHandler('cancel', cancel)]
--- localisation.py
+++ localisation.py
@@ -1,5 +1,11 @@
 langs = {
-"en_US": {"screen": "the screen is broken",
+"en_US": {
+	"from1400": "from 1400",
+	"from1500": "from 1500",
+	"from1600": "from 1600",
+	"from2000": "from 2000",
+	"from3500": "from 3500",
+	"screen": "the screen is broken",
 	"button": "the button is broken",
 	"liquid": "liquid got inside",
 	"cam": "cam is broken",
@@ -13,13 +19,13 @@ langs = {
 	"thirty_min": "30 minutes",
 	"forty_min": "40 minutes",
 	"four_hours": "from 4 hours",
-	"hello": "Hello, %s, I'm an AppleCrash bot (applecrash.ru), I can help you to know the cost of repair of iPhone and iPad.\nCould you tell me, what device you have?",
-	"ask_model": "Excellent! So, you need repair of %s, what mark do you have?",
-	"ask_fault": "You have %s %s, what is exactly wrong with it?",
+	"hello": "Hello, {}, I'm an AppleCrash bot (applecrash.ru), I can help you to know the cost of repair of iPhone and iPad.\nCould you tell me, what device you have?",
+	"ask_model": "Excellent! So, you need repair of {}, what mark do you have?",
+	"ask_fault": "You have {} {}, what is exactly wrong with it?",
 	"if_dont_know": "If you don't know, what is exactly wrong with it, our master can come to you and check your device for free\nIf you agree, press \"Yes\"",
-	"iphone_after_eight": "You have iPhone %s, they have been released on the Russian market just recently, that means that prices for the parts have to be checked, press \"Yes\" to make request for the cost",
-	"result_with_screen": "If %s, the cost will be %s rubles + protective glass as a present!\nThe guarantee is from %s. The repair will take about %s\nThe cost includes visit of our master to your place\nIf you agree, press \"Yes\"",
-	"resulting": "If %s, the cost will be %s rubles.\nThe guarantee is from %s. The repair will take about %s\nThe cost includes visit of our master to your place\nIf you agree, press \"Yes\"",
+	"iphone_after_eight": "You have iPhone {}, they have been released on the Russian market just recently, that means that prices for the parts have to be checked, press \"Yes\" to make request for the cost",
+	"result_with_screen": "If {}, the cost will be {} rubles + protective glass as a present!\nThe guarantee is from {}. The repair will take about {}\nThe cost includes visit of our master to your place\nIf you agree, press \"Yes\"",
+	"resulting": "If {}, the cost will be {} rubles.\nThe guarantee is from {}. The repair will take about {}\nThe cost includes visit of our master to your place\nIf you agree, press \"Yes\"",
 	"other": "other",
 	"I_dont_know": "I don't know",
 	"start_again": "Start again",
@@ -37,8 +43,20 @@ langs = {
 	"ask_contact_promo": "For discount pleasesend your contact",
 	"thank_you_good": "Thank you! We'll contact you soon for details.\nOur contacts: @chernikovden, +79999776258 Дмитрий",
 	"thank_you_bad": "Thank you for your feedback, we'll use this information to improve our bot!\nIf you want to start again, press \"Start again\" or any text\nOur contacts: @chernikovden, +79999776258 Дмитрий",
+	"price": "Price",
+	"terms": "Terms",
+	"necessity_contacts": "The necessity to send the contacts",
+	"so_sad": "t's a pity :( In order to improve service, could you please tell us, what particularly was wrong with this service?",
+	"if_cancel": "I cant recognize your answer, sadly. Let's start again?",
 	},
-"ru_RU": {"screen": "разбился экран",
+
+"ru_RU": {
+	"from1400": "от 1400",
+	"from1500": "от 1500",
+	"from1600": "от 1600",
+	"from2000": "от 2000",
+	"from3500": "от 3500",
+	"screen": "разбился экран",
 	"button": "сломалась кнопка",
 	"liquid": "попала жидкость",
 	"cam": "cломалась камера",
@@ -52,13 +70,13 @@ langs = {
 	"thirty_min": "30 минут",
 	"forty_min": "40 минут",
 	"four_hours": "от 4-х часов",
-	"hello": "Привет, %s, я бот AppleCrash (applecrash.ru) - Ваш помощник по вопросам быстрого ремонта техники Apple в Москве и Московской области.\nНиже вы можете увидеть, что я могу:",
-	"ask_model": "Отлично! Вам нужен ремонт %s, какая у Вас модель?",
-	"ask_fault": "У Вас %s %s, что именно неисправно?",
+	"hello": "Привет, {}, я бот AppleCrash (applecrash.ru) - Ваш помощник по вопросам быстрого ремонта техники Apple в Москве и Московской области.\nНиже вы можете увидеть, что я могу:",
+	"ask_model": "Отлично! Вам нужен ремонт {}, какая у Вас модель?",
+	"ask_fault": "У Вас {} {}, что именно неисправно?",
 	"if_dont_know": "Если Вы не знаете, что именно у Вас неисправно, к Вам может выехать мастер и провести бесплатную диагностику\nЕсли Вас это устраивает, нажмите \"Да\"",
-	"iphone_after_eight": "У Вас iPhone %s, в России они поступили в продажу совсем недавно, а это значит, что цены на запчасти нужно будет уточнять, нажмите \"Да\", чтобы оставить запрос цены",
-	"result_with_screen": "Если у Вас %s, то ремонт у нас будет стоить %s рублей + защитное стекло в подарок!\nГарантия %s. Ремонт займёт примерно %s\nВ цену входит выезд мастера в удобное для Вас место!\nЕсли Вас это устраивает, нажмите \"Да\"",
-	"resulting": "Если у Вас %s, то ремонт у нас будет стоить %s рублей.\nГарантия от %s. Ремонт займёт примерно %s\nВ цену входит выезд мастера в удобное для Вас место!\nЕсли Вас это устраивает, нажмите \"Да\"",
+	"iphone_after_eight": "У Вас iPhone {}, в России они поступили в продажу совсем недавно, а это значит, что цены на запчасти нужно будет уточнять, нажмите \"Да\", чтобы оставить запрос цены",
+	"result_with_screen": "Если у Вас {}, то ремонт у нас будет стоить {} рублей + защитное стекло в подарок!\nГарантия {}. Ремонт займёт примерно {}\nВ цену входит выезд мастера в удобное для Вас место!\nЕсли Вас это устраивает, нажмите \"Да\"",
+	"resulting": "Если у Вас {}, то ремонт у нас будет стоить {} рублей.\nГарантия от {}. Ремонт займёт примерно {}\nВ цену входит выезд мастера в удобное для Вас место!\nЕсли Вас это устраивает, нажмите \"Да\"",
 	"other": "другое",
 	"I_dont_know": "я не знаю",
 	"start_again": "Начать сначала",
@@ -76,13 +94,9 @@ langs = {
 	"ask_contact_promo": "Для получения скидки прямо сейчас оставьте свой контакт",
 	"thank_you_good": "Спасибо! В ближайшее время с Вами свяжется менеджер для уточнения деталей.\nОбратная связь: @chernikovden, +79999776258 Дмитрий",
 	"thank_you_bad": "Спасибо за отзыв, мы обязательно используем эту информацию для улучшения нашего бота в будущем!\nЕсли хотите попробовать снова, нажмите \"Начать сначала\" или отправьте любой текст\nОбратная связь: @chernikovden, +79999776258 Дмитрий",
+	"price": "Цена",
+	"terms": "Сроки",
+	"necessity_contacts": "Необходимость оставлять контактные данные",
+	"so_sad": "Жаль :( В целях улучшения бота, скажите, что именно Вас не устроило?",
+	"if_cancel": "К сожалению, я не могу распознать введённый Вами текст, давайте начнём сначала?",
 	}}
-iphone_after_eight = "У Вас iPhone %s, в России они поступили в продажу совсем недавно, а это значит, что цены на запчасти нужно будет уточнять, нажмите \"Да\", чтобы оставить запрос цены"
-result_with_screen = "Если у Вас %s, то ремонт у нас будет стоить %s рублей + защитное стекло в подарок!\nГарантия от %s. Ремонт займёт примерно %s\nВ цену входит выезд мастера в удобное для Вас место!\nЕсли Вас это устраивает, нажмите \"Да\""
-resulting = "Если у Вас %s, то ремонт у нас будет стоить %s рублей.\nГарантия от %s. Ремонт займёт примерно %s\nВ цену входит выезд мастера в удобное для Вас место!\nЕсли Вас это устраивает, нажмите \"Да\""
-ask_contact = "Отлично! Оставьте, пожалуйста, свой контакт для связи"
-price = "Цена"
-terms = "Сроки"
-necessity_contacts = "Необходимость оставлять контактные данные"
-so_sad = "Жаль :( В целях улучшения бота, скажите, что именно Вас не устроило?"
-if_cancel = "К сожалению, я не могу распознать введённый Вами текст, давайте начнём сначала?"
